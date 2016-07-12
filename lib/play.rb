1864e67d30874853e315bc7f76040120c8c17402
Mohammed Omar <samiomarmohammed@gmail.com>
2016-07-12 17:41:28 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -35,4 +35,22 @@ def turn(board)
   end
 end
 
+# so this is interesting because if you set the play loop to equal 9, then turn runs 10 times
+# what this does is it causes the board to fill up and then have one more turn but the valid move
+# method doesnt work so turn just keeps looping, from the "else section of the turn method."
 # Define your play method below
+# def play(board)
+#   counter = 0
+#   until counter == 9
+#     turn(board)
+#     counter += 1
+#   end
+# end
+
+def play(board)
+  counter = 0
+  while counter < 9
+    turn(board)
+    counter += 1
+  end
+end
